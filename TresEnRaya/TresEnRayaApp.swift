8fe9b4cfc80379b109a51773c96fe5e27c000f1d
Edgar <edguitian@imagine800.es>
2025-04-11 10:51:34 +0200

    Initial Commit

---
+++ TresEnRaya/TresEnRayaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TresEnRayaApp.swift
+//  TresEnRaya
+//
+//  Created by Edgar Guitian Rey on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct TresEnRayaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
